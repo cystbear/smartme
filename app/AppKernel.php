4ae34d60de5383ef3aa5ab8be6a605f0025fe908
Oleg Zinchenko <1cdecoder@gmail.com>
2015-04-04 17:31:00 +0300

    added base empty bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+
+            new Jack\DeckKeeperBundle\JackDeckKeeperBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Jack/DeckKeeperBundle/JackDeckKeeperBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jack\DeckKeeperBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JackDeckKeeperBundle extends Bundle
+{
+}
